394a64a31b14d295c97da6ab96b02da48fe2bb00
Кирилл Толмачев <stitch00007@gmail.com>
2023-03-09 18:24:04 +0700

    fixed download and gradle build
    https://github.com/blblblblblblblblbl/Unsplash-API-app/issues/72 fixed

--- myProject/feature/detailedphoto/src/main/java/com/blblblbl/detailedphoto/ui/DownloadNotificationsImpl.kt
+++ myProject/feature/detailedphoto/src/main/java/com/blblblbl/detailedphoto/ui/DownloadNotificationsImpl.kt
@@ -1,10 +1,13 @@
 package com.blblblbl.detailedphoto.ui
 
+import android.Manifest
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Build
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.blblblbl.detailedphoto.R
@@ -17,13 +20,27 @@ class DownloadNotificationsImpl @Inject constructor(): DownloadNotifications {
         makeChannel(context)
         // Create the notification
         val builder = NotificationCompat.Builder(context, CHANNEL_ID)
-            //.setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
             .setContentTitle(NOTIFICATION_TITLE)
             .setContentText(message)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
             .setVibrate(LongArray(0))
 
         // Show the notification
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
         NotificationManagerCompat.from(context).notify(NOTIFICATION_ID, builder.build())
     }
     private fun makeChannel(context: Context){
@@ -48,7 +65,7 @@ class DownloadNotificationsImpl @Inject constructor(): DownloadNotifications {
         makeChannel(context)
              // Create the notification
              val builder = NotificationCompat.Builder(context, CHANNEL_ID)
-                 //.setSmallIcon(R.drawable.ic_launcher_foreground)
+                 .setSmallIcon(R.drawable.ic_launcher_foreground)
                  .setContentTitle(NOTIFICATION_TITLE)
                  .setContentText(message)
                  .setContentIntent(intent)
@@ -56,7 +73,21 @@ class DownloadNotificationsImpl @Inject constructor(): DownloadNotifications {
                  .setVibrate(LongArray(0))
 
              // Show the notification
-             NotificationManagerCompat.from(context).notify(NOTIFICATION_ID, builder.build())
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        NotificationManagerCompat.from(context).notify(NOTIFICATION_ID, builder.build())
     }
 }
 @JvmField val VERBOSE_NOTIFICATION_CHANNEL_NAME: CharSequence = "Verbose WorkManager Notifications"
